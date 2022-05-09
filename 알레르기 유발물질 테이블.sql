aa330c63ecfff15d788e2dfea46e96ef09087c64
OH-JOU-YEON <91864044+OH-JOU-YEON@users.noreply.github.com>
2022-05-09 22:19:11 +0900

    알레르기 유발물질 테이블 입니다

---
+++ 알레르기 유발물질 테이블.sql
@@ -0,0 +1,59 @@
+CREATE TABLE 유발물질 (
+고유번호 NUMBER,
+이름 VARCHAR2(20),
+PRIMARY KEY(고유번호));
+
+INSERT INTO 유발물질(고유번호, 이름)
+VALUES(3,'메밀');
+
+INSERT INTO 유발물질(고유번호, 이름)
+VALUES(6,'밀');
+
+INSERT INTO 유발물질(고유번호, 이름)
+VALUES(5,'대두');
+
+INSERT INTO 유발물질(고유번호, 이름)
+VALUES(14,'호두');
+
+INSERT INTO 유발물질(고유번호, 이름)
+VALUES(4,'땅콩');
+
+INSERT INTO 유발물질(고유번호, 이름)
+VALUES(11,'복숭아');
+
+INSERT INTO 유발물질(고유번호, 이름)
+VALUES(12,'토마토');
+
+INSERT INTO 유발물질(고유번호, 이름)
+VALUES(10,'돼지고기');
+
+INSERT INTO 유발물질(고유번호, 이름)
+VALUES(1,'난류(계란)');
+
+INSERT INTO 유발물질(고유번호, 이름)
+VALUES(2,'우유');
+
+INSERT INTO 유발물질(고유번호, 이름)
+VALUES(15,'닭고기');
+
+INSERT INTO 유발물질(고유번호, 이름)
+VALUES(16,'쇠고기');
+
+INSERT INTO 유발물질(고유번호, 이름)
+VALUES(9,'새우');
+
+INSERT INTO 유발물질(고유번호, 이름)
+VALUES(7,'고등어');
+
+INSERT INTO 유발물질(고유번호, 이름)
+VALUES(8,'게');
+
+INSERT INTO 유발물질(고유번호, 이름)
+VALUES(13,'아황산류');
+
+INSERT INTO 유발물질(고유번호, 이름)
+VALUES(17,'오징어');
+
+INSERT INTO 유발물질(고유번호, 이름)
+VALUES(18,'조개류(어패류)');
+
