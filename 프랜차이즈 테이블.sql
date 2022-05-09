bfc3a7fae936dc0c2e9b9a514c7aadbbd472856f
OH-JOU-YEON <91864044+OH-JOU-YEON@users.noreply.github.com>
2022-05-09 22:01:56 +0900

    프랜차이즈 테이블입니다

---
+++ 프랜차이즈 테이블.sql
@@ -0,0 +1,54 @@
+CREATE TABLE 프랜차이즈 (
+고유번호 NUMBER,
+이름 VARCHAR2(20),
+PRIMARY KEY(고유번호));
+
+INSERT INTO 프랜차이즈(고유번호, 이름)
+VALUES(1,'BBQ');
+
+INSERT INTO 프랜차이즈(고유번호, 이름)
+VALUES(2,'BHC');
+
+INSERT INTO 프랜차이즈(고유번호, 이름)
+VALUES(3,'공차');
+
+INSERT INTO 프랜차이즈(고유번호, 이름)
+VALUES(4,'던킨도넛');
+
+INSERT INTO 프랜차이즈(고유번호, 이름)
+VALUES(15,'홍루이젠');
+
+INSERT INTO 프랜차이즈(고유번호, 이름)
+VALUES(5,'도미노피자');
+
+INSERT INTO 프랜차이즈(고유번호, 이름)
+VALUES(6,'맥도날드');
+
+INSERT INTO 프랜차이즈(고유번호, 이름)
+VALUES(7,'명랑 핫도그');
+
+INSERT INTO 프랜차이즈(고유번호, 이름)
+VALUES(8,'배스킨라빈스');
+
+INSERT INTO 프랜차이즈(고유번호, 이름)
+VALUES(9,'본죽');
+
+INSERT INTO 프랜차이즈(고유번호, 이름)
+VALUES(10,'샐러디');
+
+INSERT INTO 프랜차이즈(고유번호, 이름)
+VALUES(11,'스타벅스');
+
+INSERT INTO 프랜차이즈(고유번호, 이름)
+VALUES(12,'와플대학');
+
+INSERT INTO 프랜차이즈(고유번호, 이름)
+VALUES(13,'파리바게트');
+
+INSERT INTO 프랜차이즈(고유번호, 이름)
+VALUES(14,'한솥');
+
+SELECT *
+FROM 프랜차이즈;
+
+
