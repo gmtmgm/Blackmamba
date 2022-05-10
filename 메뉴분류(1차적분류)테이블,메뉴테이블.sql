ca13af4b34fd95c433797d826e5b8aff9dbbef38
OH-JOU-YEON <91864044+OH-JOU-YEON@users.noreply.github.com>
2022-05-10 20:21:59 +0900

    Add files via upload

--- 메뉴분류(1차적분류)테이블,메뉴테이블.sql
+++ 메뉴분류(1차적분류)테이블,메뉴테이블.sql
@@ -9,7 +9,7 @@ CREATE TABLE 메뉴(
 프랜차이즈번호 NUMBER NOT NULL,
 분류번호 NUMBER NOT NULL,
 메뉴고유번호 NUMBER NOT NULL,
-메뉴이름 VARCHAR2(20) NOT NULL,
+메뉴이름 VARCHAR2(50) NOT NULL,
 유발물질번호 NUMBER,
 PRIMARY KEY(프랜차이즈번호,분류번호,메뉴고유번호,유발물질번호),
 FOREIGN KEY(프랜차이즈번호,분류번호) REFERENCES 메뉴분류(프랜차이즈번호,분류번호),
