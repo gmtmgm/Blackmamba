0a83e0fe2d4ccce2108e2145028996a9d8fd68c0
OH-JOU-YEON <91864044+OH-JOU-YEON@users.noreply.github.com>
2022-05-14 20:01:45 +0900

    샐러디 메뉴테이블입니다

---
+++ 샐러디 메뉴.sql
@@ -0,0 +1,205 @@
+INSERT INTO 메뉴분류(프랜차이즈번호,분류번호,분류이름)
+VALUES(10,1,'웜볼');
+
+INSERT INTO 메뉴분류(프랜차이즈번호,분류번호,분류이름)
+VALUES(10,2,'샐러디');
+
+INSERT INTO 메뉴분류(프랜차이즈번호,분류번호,분류이름)
+VALUES(10,3,'샌드');
+
+INSERT INTO 메뉴분류(프랜차이즈번호,분류번호,분류이름)
+VALUES(10,4,'랩');
+
+INSERT INTO 메뉴분류(프랜차이즈번호,분류번호,분류이름)
+VALUES(10,5,'웜랩');
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,1,1,'칠리베이컨',12);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,1,1,'칠리베이컨',10);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,1,1,'칠리베이컨',1);
+
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,1,2,'우삼겹 BEEF LOIN',12);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,1,2,'우삼겹 BEEF LOIN',4);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,1,2,'우삼겹 BEEF LOIN',14);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,1,2,'우삼겹 BEEF LOIN',16);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,1,3,'팔라펠단호박 FALAFEL SWEET PUMPKIN',12);
+
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,1,4,'고추장머쉬룸 GOCHUJANG MUSHROOM',12);
+
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,1,5,'할라피뇨치킨 JALAPENO CHICKEN',15);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,1,5,'할라피뇨치킨 JALAPENO CHICKEN',12);
+
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,2,1,'시저치킨 CAESAR CHICKEN',15);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,2,1,'시저치킨 CAESAR CHICKEN',2);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,2,1,'시저치킨 CAESAR CHICKEN',12);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,2,2,'탄단지 TANDANJI',15);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,2,2,'탄단지 TANDANJI',12);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,2,2,'탄단지 TANDANJI',2);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,2,2,'탄단지 TANDANJI',14);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,2,2,'탄단지 TANDANJI',4);
+
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,2,3,'콥 COBB',15);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,2,3,'콥 COBB',1);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,2,3,'콥 COBB',12);
+
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,2,4,'리코타치즈 RICOTTA CHEESE',14);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,2,4,'리코타치즈 RICOTTA CHEESE',4);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,2,4,'리코타치즈 RICOTTA CHEESE',2);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,2,4,'리코타치즈 RICOTTA CHEESE',12);
+
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,2,5,'연어 SALMON',12);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,2,5,'연어 SALMON',2);
+
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,2,6,'우삼겹메밀면 BEEF LOIN BUCKWHEAT NOODLES',4);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,2,6,'우삼겹메밀면 BEEF LOIN BUCKWHEAT NOODLES',14);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,2,6,'우삼겹메밀면 BEEF LOIN BUCKWHEAT NOODLES',16);
+
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,3,1,'치킨토마토 CHICKEN TOMATO',2);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,3,1,'치킨토마토 CHICKEN TOMATO',5);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,3,1,'치킨토마토 CHICKEN TOMATO',12);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,3,1,'치킨토마토 CHICKEN TOMATO',1);
+
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,3,2,'에그베이컨 EGGBACON',2);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,3,2,'에그베이컨 EGGBACON',1);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,3,2,'에그베이컨 EGGBACON',10);
+
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,3,3,'크리미연어 CREAMYSALMON',2);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,3,3,'크리미연어 CREAMYSALMON',1);
+
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,4,1,'멕시칸 MEXICAN',2);
+
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,4,2,'시저치킨 CAESAR CHICKEN',2);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,4,2,'시저치킨 CAESAR CHICKEN',15);
+
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,4,3,'크렌베리그린 CRANBERRY GREEK',2);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,4,3,'크렌베리그린 CRANBERRY GREEK',12);
+
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,4,4,'크리미연어 CREAMY SALMON',2);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,4,4,'크리미연어 CREAMY SALMON',12);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,5,1,'칠리베이컨 CHILLI BACON',2);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,5,1,'칠리베이컨 CHILLI BACON',1);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,5,1,'칠리베이컨 CHILLI BACON',10);
+
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,5,2,'우삼겹 BEEF LOIN',2);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,5,2,'우삼겹 BEEF LOIN',4);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,5,2,'우삼겹 BEEF LOIN',14);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,5,2,'우삼겹 BEEF LOIN',16);
+
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,5,3,'할라피뇨치킨 JALAPENO CHICKEN',2);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,5,3,'할라피뇨치킨 JALAPENO CHICKEN',15);
+
+INSERT INTO 메뉴(프랜차이즈번호,분류번호,메뉴고유번호,메뉴이름,유발물질번호)
+VALUES(10,5,4,'팔라펠 FALAFEL',2);
+
+
+
