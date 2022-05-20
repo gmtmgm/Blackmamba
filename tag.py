2b245b3f6d7ce1a0075cb231281a9dbf1a99b3cd
gmtmgm <70586129+gmtmgm@users.noreply.github.com>
2022-05-20 17:16:00 +0900

    Update tag.py

--- tag.py
+++ tag.py
@@ -1,9 +1,13 @@
 from konlpy.tag import Komoran
+import os
 
-komoran = Komoran(userdic='./user_dic.tsv')
+def datatokenize(data: str):
+    #프랜차이즈 목록, 메뉴 이름, 알레르기 성분 입력 품사 태깅
+    komoran = Komoran(userdic= './user_dic.txt')
+
+    datatoken = komoran.pos(data)
+    return datatoken
+
 text = "프랜차이즈 목록을 살펴보고 있습니다."
 text2 = "우유는 맛있습니다."
-pos = komoran.pos(text)
-print(pos)
-pos = komoran.pos(text2)
-print(pos)
+print(datatokenize(text))
