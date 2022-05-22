0fb17d16ea2a0b069bf4a8beff98edd35fa30fba
gmtmgm <70586129+gmtmgm@users.noreply.github.com>
2022-05-23 03:28:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,92 @@
 import tensorflow as tf
 from tag import *
-from tensorflow import keras
-import csv
+import pandas as pd
+from tensorflow.keras import preprocessing
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Embedding, Dense, Dropout, Conv1D, GlobalMaxPool1D, concatenate
 
-q_sentences = [] #입력받은 질문 문장(우리 챗봇의 경우 리스트일 필요 없으니 추후 수정)
-a_sentences = [] #답변 문장
-sen_tag = [] #답변 태그
-q_token = [] #질문 토크나이징
+q_sentences = [] 
+a_sentences = [] 
+sen_tag = [] 
+q_token = [] 
 
-""" 입력 데이터 연동 과정 후 실행 """
+train_file = "./ChatbotData.csv"
+data = pd.read_csv(train_file, delimiter=',')
+features = data['Q'].tolist()
+labels = data['label'].tolist()
 
+corpus = [preprocessing.text.text_to_word_sequence(text) for text in features]
+tokenizer = preprocessing.text.Tokenizer()
+tokenizer.fit_on_texts(corpus)
+sequences = tokenizer.texts_to_sequences(corpus)
+word_index = tokenizer.word_index
+
+MAX_SEQ_LEN = 15
+padded_seqs = preprocessing.sequence.pad_sequences(sequences, maxlen=MAX_SEQ_LEN, padding='post')
+
+ds = tf.data.Dataset.from_tensor_slices((padded_seqs, labels))
+ds = ds.shuffle(len(features))
+
+train_size = int(len(padded_seqs) * 0.7)
+val_size = int(len(padded_seqs) * 0.2)
+test_size = int(len(padded_seqs) * 0.1)
+
+train_ds = ds.take(train_size).batch(20)
+val_ds = ds.skip(train_size).take(val_size).batch(20)
+test_ds = ds.skip(train_size + val_size).take(test_size).batch(20)
+
+dropout_prob = 0.5
+EMB_SIZE = 128
+EPOCH = 5
+VOCAB_SIZE = len(word_index) + 1
+
+input_layer = Input(shape=(MAX_SEQ_LEN,))
+embedding_layer = Embedding(VOCAB_SIZE, EMB_SIZE, input_length=MAX_SEQ_LEN)(input_layer)
+dropout_emb = Dropout(rate=dropout_prob)(embedding_layer)
+
+conv1 = Conv1D(
+    filters=128, 
+    kernel_size=3,
+    padding='valid',
+    activation=tf.nn.relu)(dropout_emb)
+pool1 = GlobalMaxPool1D()(conv1)
+
+conv2 = Conv1D(
+    filters=128, 
+    kernel_size=4,
+    padding='valid',
+    activation=tf.nn.relu)(dropout_emb)
+pool2 = GlobalMaxPool1D()(conv2)
+
+conv3 = Conv1D(
+    filters=128, 
+    kernel_size=5,
+    padding='valid',
+    activation=tf.nn.relu)(dropout_emb)
+pool3 = GlobalMaxPool1D()(conv3)
+
+concat = concatenate([pool1, pool2, pool3])
+
+hidden = Dense(128, activation=tf.nn.relu)(concat)
+dropout_hidden = Dropout(rate=dropout_prob)(hidden)
+logits = Dense(3, name='logits')(dropout_hidden)
+predictions = Dense(3, activation=tf.nn.softmax)(logits)
+
+model = Model(inputs=input_layer, outputs=predictions)
+model.compile(optimizer='adam',
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
+
+model.fit(train_ds, validation_data=val_ds, epochs=EPOCH, verbose=1)
+
+loss, accuracy = model.evaluate(test_ds, verbose=1)
+print('Accuracy: %f' % (accuracy * 100))
+print('loss: %f' % (loss))
+
+model.save('cnn_model.h5')
+
+
+"""
 for line in readdata:
     q_sentences.append(line['query'])
     a_sentences.append(line['answer'])
@@ -18,19 +95,4 @@ for line in readdata:
 
 for data in q_sentences:
     q_token.append(tokendatatokenize(data))
-
-""" 품사 확인 후 메뉴 데이터베이스에서 비교 """
-#나중에 데이터베이스 연결 파일 쪽으로 분리해야 할 듯
-
-for token in q_token:
-    if q_token[token][1] == 'FRL': #프랜차이즈 명을 알려달라는 입력을 받음
-        """SELECT 이름
-        FROM 프랜차이즈"""
-    elif q_token[token][1] == 'FRC': #특정 프랜차이즈 명을 입력받음        
-        """ """
-    #메뉴 명을 알려달라는 입력을 받음
-    #특정 메뉴 명을 입력받음
-    #특정 알레르기 성분을 입력받음
-    #특정 메뉴와 특정 알레르기 성분을 입력받음
-    #특정 알레르기 성분을 2개 이상 입력받음
-    
+"""
