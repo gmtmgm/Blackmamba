f274ec3192b75b8d3b893bfebb1c66e7fec62dad
gmtmgm <70586129+gmtmgm@users.noreply.github.com>
2022-05-24 00:02:43 +0900

    Update DB_connect.py

--- DB_connect.py
+++ DB_connect.py
@@ -1,13 +1,13 @@
 import cx_Oracle as cx
 
 dsn = cx.makedsn("localhost", 1521, service_name= "XE")
-conn = cx.connect(user="c##Blackmamba", password="blackmamba", dsn=dsn)
+db = cx.connect(user="c##Blackmamba", password="blackmamba", dsn=dsn)
 
-cur = conn.cursor()
+cur = db.cursor()
 cursor = cur.execute("select * from 프랜차이즈")
 
 for i in cursor:
     print(i)
 
 cur.close()
-conn.close()
+db.close()
