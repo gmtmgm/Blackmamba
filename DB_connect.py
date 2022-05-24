e39d2fe956f9e0c506177261af6f97bc429eb04e
gmtmgm <70586129+gmtmgm@users.noreply.github.com>
2022-05-25 07:41:47 +0900

    Update DB_connect.py

--- DB_connect.py
+++ DB_connect.py
@@ -1,13 +1,52 @@
+
 import cx_Oracle as cx
 
-dsn = cx.makedsn("localhost", 1521, service_name= "XE")
-db = cx.connect(user="c##Blackmamba", password="blackmamba", dsn=dsn)
+class Database:
 
-cur = db.cursor()
-cursor = cur.execute("select * from 프랜차이즈")
+    def __init__(self, host, port, service_name, user, password):
+        self.host = host
+        self.port = port
+        self.service_name = service_name
+        self.user = user
+        self.password = password
+        self.conn = None
+    
+    def connect(self):
+        if self.conn != None:
+            return
+        self.conn = cx.connect(
+            user=self.user,
+            password=self.password,
+            dsn=cx.makedsn(self.host, self.port, self.service_name)
+        )
+    
+    def close(self):
+        if self.conn is None:
+            return 
+        self.conn.close()
+        self.conn = None
+    
+    def exedata(self, sql):
+        data = []
+        cur = self.conn.cursor()
+        cursor = cur.execute(sql)
+        for i in cursor:
+            for ii in i:
+                data.append(ii)
+        cur.close()
+        return data
 
-for i in cursor:
-    print(i)
 
-cur.close()
+db = Database(
+    host="localhost", port=1521, service_name="XE", 
+    user="c##Blackmamba", password="blackmamba"
+)
+
+"""
+테스트
+db.connect()
+
+sql = "select * from 프랜차이즈"
+print(db.exedata(sql))
 db.close()
+"""
